a6fa716052e1755fa7f5cd62955127dc4dc2cb04
GilsonCordeiro <gil.silva.cord2021@gmail.com>
2022-10-26 22:11:04 -0300

    project restructuring

--- weather-react/src/components/Header.js
+++ weather-react/src/components/Header.js
@@ -3,8 +3,8 @@ function Header() {
     return (
       <>
         <nav class="navbar navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">
-            Consulta de Previsão do tempo
+            <a class="navbar-brand" href="#top">
+            # Previsão do tempo
             </a>
         </nav>
       </>
--- weather-react/src/components/Jumbotron.js
+++ weather-react/src/components/Jumbotron.js
@@ -1,12 +1,20 @@
 function Jumbotron(){
     return(
-        <div>
-            <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Fluid jumbotron</h1>
-                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-            </div>
-            </div>
+        <div>         
+                <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Verifique agora a previsão do tempo da sua cidade!</h1>
+                    <p className="lead">Digite o nome de sua cidade abaixo e em seguida clique em pesquisar </p>
+                    <div className= "row mb-4">
+                        <div className="col-md-6">
+                            <input className="form-control"/>
+                        </div>
+                    </div>
+                    <button className="btn btn-primary" >
+                        Pesquisar
+                    </button>
+                </div>
+                </div> 
         </div>
 
     )
