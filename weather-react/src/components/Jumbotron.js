4d1a46217a05f8ea11a6392655a8d33ebdf3b77c
GilsonCordeiro <gil.silva.cord2021@gmail.com>
2022-10-27 09:14:56 -0300

    show weather search by region

--- weather-react/src/components/Jumbotron.js
+++ weather-react/src/components/Jumbotron.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 function Jumbotron(){
     const [city, setCity] = useState("Brasília")
+    const [weathForecast, setWeathForecast] = useState(null)
 
     const handleChang = (e)=>{
         setCity(e.target.value)
@@ -12,13 +13,15 @@ function Jumbotron(){
        
         fetch(`http://api.weatherapi.com/v1/current.json?key=d5c87313143a42fea5a122841222610&q=${city}&lang=pt`)
         .then ((response ) =>{
-            console.log("response ===>", response)
+            // console.log("response ===>", response)
             if(response.status === 200){
                 return response.json()
             }
         })
         .then((data) => {
-            console.log('data ===>', data)
+            // visualizar os dados da api
+             console.log('data ===>', data)
+            setWeathForecast(data)
         });
     }
 
@@ -39,6 +42,23 @@ function Jumbotron(){
                     <button onClick={handleSearch} className="btn btn-primary" >
                         Pesquisar
                     </button>
+                    {
+                        weathForecast ? (
+                            <div>
+                        <div className= "mt-4 d-flex ">
+                            <div>
+                                <img src= {weathForecast.current.condition.icon}/>
+                            </div>
+                            <div>
+                                <h1>{weathForecast.location.name}</h1>
+                                <p>{weathForecast.location.region}</p>
+                                <h3>Hoje o dia está: {weathForecast.current.condition.text}</h3>
+                                <p>Temperatura: {weathForecast.current.temp_c} º</p>
+                            </div>
+                        </div>
+                    </div>
+                        ): null
+                    }
                 </div>
                 </div> 
         </div>
